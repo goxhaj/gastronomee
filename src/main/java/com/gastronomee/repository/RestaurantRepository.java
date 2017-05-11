bf360658315c97404b03e66d525e115dfcfcc252
ardi <goxhaj@gmail.com>
2017-05-11 22:52:01 +0200

    home page, with elastic search

--- src/main/java/com/gastronomee/repository/RestaurantRepository.java
+++ src/main/java/com/gastronomee/repository/RestaurantRepository.java
@@ -1,18 +1,22 @@
 package com.gastronomee.repository;
 
-import com.gastronomee.domain.Restaurant;
-
-import org.springframework.data.jpa.repository.*;
-
 import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.gastronomee.domain.Restaurant;
 
 /**
  * Spring Data JPA repository for the Restaurant entity.
  */
-@SuppressWarnings("unused")
 public interface RestaurantRepository extends JpaRepository<Restaurant,Long> {
 
     @Query("select restaurant from Restaurant restaurant where restaurant.manager.login = ?#{principal.username}")
     List<Restaurant> findByManagerIsCurrentUser();
+
+	Page<Restaurant> findByNameIgnoreCaseContaining(String name, Pageable page);
 
 }
--- src/main/webapp/app/home/home.controller.js
+++ src/main/webapp/app/home/home.controller.js
@@ -5,17 +5,33 @@
         .module('gastronomeeApp')
         .controller('HomeController', HomeController);
 
-    HomeController.$inject = ['$scope', 'Principal', 'LoginService', '$state'];
+    HomeController.$inject = ['$scope', 'Principal', 'LoginService', 'Restaurant', 'RestaurantSearch', '$state', 'ParseLinks', 'paginationConstants', 'pagingParams'];
 
-    function HomeController ($scope, Principal, LoginService, $state) {
+    function HomeController ($scope, Principal, LoginService, Restaurant, RestaurantSearch, $state, ParseLinks, paginationConstants, pagingParams) {
         var vm = this;
-
+        
         vm.account = null;
         vm.isAuthenticated = null;
         vm.login = LoginService.open;
         vm.register = register;
+        
+        vm.restaurants = [];
+        
+        vm.loadPage = loadPage;
+        vm.predicate = pagingParams.predicate;
+        vm.reverse = pagingParams.ascending;
+        vm.transition = transition;
+        vm.itemsPerPage = 5;//paginationConstants.itemsPerPage
+        vm.clear = clear;
+        vm.search = search;
+        vm.loadAll = loadAll;
+        vm.searchQuery = pagingParams.search;
+        vm.currentSearch = pagingParams.search;
+
+        loadAll();
+        
         $scope.$on('authenticationSuccess', function() {
-            getAccount();
+            getAccount();        
             $state.go("dashboard");
         });
 
@@ -29,6 +45,75 @@
         }
         function register () {
             $state.go('register');
+        }        
+
+        function loadAll () {
+            if (pagingParams.search) {
+                RestaurantSearch.query({
+                    query: pagingParams.search,
+                    page: pagingParams.page - 1,
+                    size: vm.itemsPerPage,
+                    sort: sort()
+                }, onSuccess, onError);
+            } else {
+                Restaurant.query({
+                    page: pagingParams.page - 1,
+                    size: vm.itemsPerPage,
+                    sort: sort()
+                }, onSuccess, onError);
+            }
+            function sort() {
+                var result = [vm.predicate + ',' + (vm.reverse ? 'asc' : 'desc')];
+                if (vm.predicate !== 'id') {
+                    result.push('id');
+                }
+                return result;
+            }
+            function onSuccess(data, headers) {
+                vm.links = ParseLinks.parse(headers('link'));
+                vm.totalItems = headers('X-Total-Count');
+                vm.queryCount = vm.totalItems;
+                vm.restaurants = data;
+                vm.page = pagingParams.page;
+            }
+            function onError(error) {
+                AlertService.error(error.data.message);
+            }
         }
+
+        function loadPage(page) {
+            vm.page = page;
+            vm.transition();
+        }
+
+        function transition() {
+            $state.transitionTo($state.$current, {
+                page: vm.page,
+                sort: vm.predicate + ',' + (vm.reverse ? 'asc' : 'desc'),
+                search: vm.currentSearch
+            });
+        }
+
+        function search(searchQuery) {
+            if (!searchQuery){
+                return vm.clear();
+            }
+            vm.links = null;
+            vm.page = 1;
+            vm.predicate = '_score';
+            vm.reverse = false;
+            vm.currentSearch = searchQuery;
+            vm.transition();
+        }
+
+        function clear() {
+            vm.links = null;
+            vm.page = 1;
+            vm.predicate = 'id';
+            vm.reverse = true;
+            vm.currentSearch = null;
+            vm.transition();
+        }
+        
     }
 })();
