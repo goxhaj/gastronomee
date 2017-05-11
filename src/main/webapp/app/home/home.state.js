06109b34d294a225d8a5272485bd3da9675ee3a9
ardi <goxhaj@gmail.com>
2017-05-11 21:32:24 +0200

    added a dashboard to be shown on login and added client side roles for pages, updated the menu based on roles.

--- src/main/webapp/app/home/home.state.js
+++ src/main/webapp/app/home/home.state.js
@@ -27,6 +27,26 @@
                     return $translate.refresh();
                 }]
             }
+        })
+        .state('dashboard', {
+            parent: 'app',
+            url: '/dashboard',
+            data: {
+                authorities: ["ROLE_USER","ROLE_MANAGER","ROLE_ADMIN"]
+            },
+            views: {
+                'content@': {
+                    templateUrl: 'app/home/dashboard.html',
+                    controller: 'HomeController',
+                    controllerAs: 'vm'
+                }
+            },
+            resolve: {
+                translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate,$translatePartialLoader) {
+                    $translatePartialLoader.addPart('home');
+                    return $translate.refresh();
+                }]
+            }
         });
     }
 })();
